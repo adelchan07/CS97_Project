d0f2556a9e0bfb18591defe7bc3faf91591f9b00
mattlouss <78622870+mattlouss@users.noreply.github.com>
2021-02-13 09:49:42 -0800

    Welcome, signup, login, pages

---
+++ Login-Signup/loginForm.js
@@ -0,0 +1,8 @@
+function passwordVisible() {
+    let input = document.getElementById("pass");
+    return input.type === "password" ? input.type = "text" : input.type = "password"
+}
+
+function validate() {
+    
+}
