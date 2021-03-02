f6102522f5f0959094fec33843027b57f4d39568
Matthew Loussinian <matthew_0328@icloud.com>
2021-03-02 12:09:01 -0800

    update form, still not sending data to backend

--- src/LoginSignup/signupForm.jsx
+++ src/LoginSignup/signupForm.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default class SignupPage extends React.Component {
+  onSubmit() { 
+  
+  }
+
   render() {
     return(<>
       <meta charSet="UTF-8" />
@@ -8,14 +12,15 @@ export default class SignupPage extends React.Component {
       <link rel="stylesheet" href="forms.css" />    
       <a href="/"><h1 id="header"> My Calendar </h1></a>
       <div id="register">
-        <form action="////replace me////">
+        <form >
           <label htmlFor="register_un"> Select a username: </label>
           <input type="text" name="register_un" placeholder="Username" autoComplete="off" k required /><br /><br />
           <label htmlFor="register_pw"> Select a password: </label> 
           <input type="password" name="register_pw" placeholder="Password" required /><br />
           <label htmlFor="confirm_pw"> Confirm password: </label> 
           <input type="password" name="confirm_pw" placeholder="Confirm Password" required /><br />
-          <button type="submit" onClick="validate()"> Create Account </button>
+
+          <button onClick={ (event) => {this.onSubmit.bind(this) ; event.preventDefault()} }> Create Account </button>
           <p> Already have an account?
             <a href="LoginForm" className="to_register">Login here</a>
           </p>
